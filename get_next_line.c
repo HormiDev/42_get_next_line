5e2a9b30a9638f142a49ae4e433975c7144ef5b0
HormiDev <ivanddiegomegia@gmail.com>
2024-03-16 03:52:35 +0100

    version 3 de listas

--- get_next_line.c
+++ get_next_line.c
@@ -6,66 +6,125 @@
 /*   By: ide-dieg <ide-dieg@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 18:50:57 by ide-dieg          #+#    #+#             */
-/*   Updated: 2024/03/14 18:55:38 by ide-dieg         ###   ########.fr       */
+/*   Updated: 2024/03/16 03:51:36 by ide-dieg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*strjoin_lst(t_list *lst, char *end)
+int	ft_lstsize(t_list *lst)
 {
-	char	*str;
-	int		i;
-	int		j;
+	int	cont;
 
-	j = 0;
-	str = malloc(BUFFER_SIZE * (ft_lstsize(lst) - 1) 
-	+ (end - ft_lstlast(lst)->content) * sizeof(char));
-	if (str == 0)
+	if (lst == 0)
 		return (0);
-	while (lst != 0)
+	cont = 1;
+	while (lst -> next != 0)
 	{
-		i = 0;
-		while (lst->content + i != end && i < BUFFER_SIZE)
+		lst = lst -> next;
+		cont++;
+	}
+	return (cont);
+}
+
+int	*ft_strnchr(const char *str, int c, int len)
+{
+	char	cchar;
+	int 	cont;
+
+	if (str == 0 || len < 0)
+		return (-1);
+	cont = 0;
+	cchar = (char)c;
+	while (cont < len)
+	{
+		if (str[cont] == cchar)
 		{
-			str[j] = (char *)lst->content[i];
-			i++;
-			j++;
+			return (cont);
 		}
-		if (lst->content + i == end)
-			break ;
+		cont++;
+	}
+	return (-1);
+}
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (lst == 0)
+		return (0);
+	while (lst -> next != 0)
+	{
+		lst = lst -> next;
+	}
+	return (lst);
+}
+
+t_list	*ft_addlstnew(t_list *lst)
+{
+	t_list	*node;
+
+	node = malloc(sizeof(t_list));
+	if (node == 0)
+		return (0);
+	node -> content = malloc(sizeof(char) * BUFFER_SIZE);
+	node -> next = NULL;
+	while (lst->next != NULL)
+	{
 		lst = lst->next;
 	}
-	return (str);
+	lst->next = node;
+	return (node);
 }
 
 char	*get_next_line(int fd)
 {
-	static t_list *buffer;
-	char	*end;
+	static t_list	*buffer;
+	char			*line;
 	
 	if (buffer == 0)
+		buffer = ft_addlstnew(buffer);
+	read(fd, buffer->content, BUFFER_SIZE);
+	while (ft_strnchr(buffer->content, '\n', BUFFER_SIZE) == -1)
 	{
-		buffer = ft_lstnew(malloc(BUFFER_SIZE * sizeof(char)));
+		buffer = ft_addlstnew(buffer);
 		read(fd, buffer->content, BUFFER_SIZE);
 	}
-	while (end = ft_strchr(ft_lstlast(buffer)->content, '\n') == 0)
-	{
-		ft_lstlast(buffer)->next = ft_lstnew(malloc(BUFFER_SIZE * sizeof(char)));
-		read(fd, ft_lstlast(buffer)->content, BUFFER_SIZE);
-	}
-	end = strjoin_lst(buffer, end);
-	return(end);
+	line = malloc((ft_lstsize(buffer) - 1) * BUFFER_SIZE + 
+	ft_strnchr(buffer->content, '\n', BUFFER_SIZE) + 1);
+	// line = superjoin(buffer, line,);
+	// nuevo nodo con el contenido restante
+	// buffer = cleanbuffer(buffer);
+	// buffer = nuevo nodo con el contenido restante
+	
+	//modificar lectura para que lea la cantidad necesaria
+	
+	return (line);
 }
 
 int main()
 {
     int fd = open("pr.txt", O_RDONLY);
+	char *line;
     if (fd == -1) 
 	{
         printf("Error al abrir el archivo.\n");
         return 1;
     }
+
+
+	line = get_next_line(fd);
+	printf("%s/", line);
+	free(line);
+	line = get_next_line(fd);
+	printf("%s/", line);
+	free(line);
+	line = get_next_line(fd);
+	printf("%s/", line);
+	free(line);
+	line = get_next_line(fd);
+	printf("%s/", line);
+	free(line);
+
+	
     close(fd);
     return 0;
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ide-dieg <ide-dieg@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 18:51:16 by ide-dieg          #+#    #+#             */
-/*   Updated: 2024/03/14 21:32:23 by ide-dieg         ###   ########.fr       */
+/*   Updated: 2024/03/16 02:48:10 by ide-dieg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,6 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
-t_list	*ft_lstnew(void *content);
-t_list	*ft_lstlast(t_list *lst);
-int		ft_lstsize(t_list *lst);
-char	*ft_strchr(const char *str, int c);
-char	*ft_strdup(const char *s1);
-char	*get_next_line(int fd);
-char	*ft_strchr(const char *str, int c);
-char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
 
 #endif
