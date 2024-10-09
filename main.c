46a45b9f34cd92385976025a4941aea87d484259
HormidDev <ide-dieg@student.42madrid.com>
2024-10-09 17:01:48 +0200

    main para probar el bonus

--- main.c
+++ main.c
@@ -6,27 +6,14 @@
 /*   By: ide-dieg <ide-dieg@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 18:32:59 by ide-dieg          #+#    #+#             */
-/*   Updated: 2024/09/21 18:19:39 by ide-dieg         ###   ########.fr       */
+/*   Updated: 2024/10/09 17:01:16 by ide-dieg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include "get_next_line.h"
-
-int	ft_lstsize(t_buffer_lst *lst)
-{
-	int	cont;
-
-	if (lst == 0)
-		return (0);
-	cont = 1;
-	while (lst -> next != 0)
-	{
-		lst = lst -> next;
-		cont++;
-	}
-	return (cont);
-}
-
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+/*
 int main()
 {
 	int		cont;
@@ -36,7 +23,7 @@ int main()
 
 	cont = 1;
 	printf("\n\nprueba de get_next_line\n");
-	fd = open("Files/pr4.txt", O_RDONLY);
+	fd = open("Files/quijote.txt", O_RDONLY);
 	line = get_next_line(fd);
 	while (line != 0)
 	{
@@ -58,9 +45,58 @@ int main()
 	free(line);
 	close(fd);
 	return 0;
-}
-*/
+}*/
+// main bonus
 /*
+int	main(void)
+{
+	int fd1 = open("Files/file1", O_RDONLY);
+	int fd2 = open("Files/file2", O_RDONLY);
+	int fd3 = open("Files/file3", O_RDONLY);
+	char *line1 = get_next_line(fd1);
+	char *line2 = get_next_line(fd2);
+	char *line3 = get_next_line(fd3);
+	while (line1 != 0 || line2 != 0 || line3 != 0)
+	{
+		if (line1 != 0)
+			printf("%s\n", line1);
+		if (line2 != 0)
+			printf("%s\n", line2);
+		if (line3 != 0)
+			printf("%s\n", line3);
+		free(line1);
+		free(line2);
+		free(line3);
+		line1 = get_next_line(fd1);
+		line2 = get_next_line(fd2);
+		line3 = get_next_line(fd3);
+	}
+	free(line1);
+	free(line2);
+	free(line3);
+	close(fd1);
+	close(fd2);
+	close(fd3);
+	return (0);
+}*/
+
+/*
+
+int	ft_lstsize(t_buffer_lst *lst)
+{
+	int	cont;
+
+	if (lst == 0)
+		return (0);
+	cont = 1;
+	while (lst -> next != 0)
+	{
+		lst = lst -> next;
+		cont++;
+	}
+	return (cont);
+}
+
 int main()
 {
     t_buffer_lst *lst = malloc(sizeof(t_buffer_lst));
